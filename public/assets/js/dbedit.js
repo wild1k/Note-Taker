dde2be104df05937ab7b5ec1ec3dec821c51a97f
wild1k <cssndrchmbrln@gmail.com>
2020-08-26 17:09:12 -0700

    got the site to load on the page, next up need to get it to show styling

---
+++ public/assets/js/dbedit.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const util = require("util");
+
+const noteDatabase = "./db/db.json"
+
+// PROMISIFY
+// =============================================================
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class DB {
+    async readJSON() {
+        try {
+            const notesRaw = await readFileAsync(noteDatabase, "utf8")
+            return notesRaw ? JSON.parse(notesRaw) : []
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async writeJSON(newNoteData, currentNotes) {
+        try {
+            const { title, text } = newNoteData;
+            const newNote = { title, text, id }
+            const combineNotes = [newNote, ...currentNotes]
+            await writeFileAsync(noteDatabase, JSON.stringify(combineNotes))
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async deleteJSON(currentNotes, requestedID) {
+        try {
+            const filteredNotes = currentNotes.filter(note => {
+                if (note.id !== requestedID) {
+                    return true
+                }
+            })
+            await writeFileAsync(noteDatabase, JSON.stringify(filteredNotes))
+
+        } catch (err) {
+            throw err
+        }
+    }
+}
+
+module.exports = new DB();
---
+++ public/assets/js/routes.js
@@ -0,0 +1,47 @@
+
+// DEPENDENCIES
+// =============================================================
+const express =require("express");
+const path = require("path")
+const router = express.Router();
+const DB = require("./dbedit.js");
+
+
+// GET CALLS
+// =============================================================
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../notes.html"));
+});
+
+router.get('/api/notes', async (req, res) => {
+    res.json(await DB.readJSON())
+});
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+
+// POST CALLS
+// =============================================================
+router.post('/api/notes', async (req, res) => {
+    const newNoteData = req.body
+    const currentNotes = await DB.readJSON();
+    await DB.writeJSON(newNoteData, currentNotes)
+    res.json("Success!")
+})
+
+
+// DELETE CALLS
+// =============================================================
+router.delete('/api/notes/:id', async (req, res) => {
+    const requestedID = req.params.id;
+    const currentNotes = await DB.readJSON();
+    await DB.deleteJSON(currentNotes, requestedID)
+    res.json("Success!")
+})
+
+
+// EXPORT
+// =============================================================
+module.exports = router;
